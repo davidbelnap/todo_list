fbc24125d61ab998ee13d326a43934f2b764b7c2
David Belnap <david.belnap6@gmail.com>
2017-08-29 18:41:14 -0600

    items controller set up, features added to new, show, and index, and others

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,26 @@ class ItemsController < ApplicationController
   end
 
   def show
-  	
+  	@item = Item.find(params[:id])
   end
 
   def new
+  	@item = Item.new
   end
+
+  def create
+  	@item = Item.new(item_params)
+
+  	if @item.save
+  		redirect_to items_path
+  	else
+  		render :new
+  	end
+  end
+
+  private
+
+  	def items_params
+  		params.require(:item).permit(:title, :user, :body)
+  	end
 end
